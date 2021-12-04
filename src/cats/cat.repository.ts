841c09ec995109b0ca31fd33b347d14bfb87e6e7
leesky <leehaneul0623@gmail.com>
2021-12-04 17:11:39 +0900

    repository 패턴 적용

---
+++ src/cats/cat.repository.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Cat } from './cats.schema';
+import { CatRequestDto } from './dto/cats.request.dto';
+/*
+Service에는 비즈니스 로직을 구현하는데
+만약 DB 관련 작업이나 다른 작업들이 복잡해지면
+비즈니스 로직에 집중하는데 방해가 된다.
+따라서 Service와 DB source 사이에 Repositoty를 두어
+비즈니스 로직에 더욱 집중할 수 있도록 하고
+중복 코드를 묶는다 (Repository 패턴)
+
+=> 모듈간 책임 분리 및 비즈니스 로직 집중
+=> 서비스 레이어에서 데이터의 출처와 관계 없이
+동일한 방식으로 데이터를 접근하도록 함
+
+ex) 여러 DB source 사용하면 데이터 출처가 많아짐 
+*/
+@Injectable()
+export class CatsRespository {
+  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+
+  async existByEmail(email: string): Promise<boolean> {
+    const result = await this.catModel.exists({ email });
+    return result;
+  }
+
+  async create(cat: CatRequestDto): Promise<Cat> {
+    return await this.catModel.create(cat);
+  }
+}
--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CatsRespository } from './cat.repository';
 import { CatsController } from './cats.controller';
 import { Cat, CatSchema } from './cats.schema';
 import { CatsService } from './cats.service';
@@ -7,7 +8,7 @@ import { CatsService } from './cats.service';
 @Module({
   imports: [MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }])],
   controllers: [CatsController],
-  providers: [CatsService],
+  providers: [CatsService, CatsRespository],
   exports: [CatsService],
 })
 export class CatsModule {}
